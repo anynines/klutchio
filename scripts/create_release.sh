cef711898da1bbb42acbff0ad5b3a01a1e45fbaa
Johannes Lux <jlux@anynines.com>
2026-05-18 13:49:59 +0200

    Second version working

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 set -euo pipefail
+# set -x
 
 #####################
 #                   #
@@ -11,8 +12,10 @@ KLUTCHIO_REPO="$(git rev-parse --show-toplevel)"
 readonly KLUTCHIO_REPO
 readonly DOCS_REPO="$KLUTCHIO_REPO/../klutchio-website"
 readonly ECR_REGISTRY_ADDRESS="public.ecr.aws/w5n9a2g2"
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-readonly CURRENT_BRANCH
+CURRENT_BRANCH_KLUTCHIO="$(cd "$KLUTCHIO_REPO" && git rev-parse --abbrev-ref HEAD)"
+readonly CURRENT_BRANCH_KLUTCHIO
+CURRENT_BRANCH_DOCS="$(cd "$DOCS_REPO" && git rev-parse --abbrev-ref HEAD)"
+readonly CURRENT_BRANCH_DOCS
 
 #####################
 #                   #
@@ -20,51 +23,51 @@ readonly CURRENT_BRANCH
 #                   #
 #####################
 
-log-normal() {
+log_info() {
     echo "[$(date "+%H:%M:%S")]" "$@"
 }
 
-log-error() {
+log_error() {
     echo -e "[$(date "+%H:%M:%S")]"'\033[31m ERROR\033[0m:' "$@"
 }
 
-check-dependency() {
+check_dependency() {
     for cmd in "$@"; do
-        log-normal "Checking for dependency: $cmd"
+        log_info "Checking for dependency: $cmd"
         if ! command -v "$cmd" 1>/dev/null 2>&1; then
-            log-error "$cmd not found in \$PATH"
+            log_error "$cmd not found in \$PATH"
             exit 1
         fi
     done
 }
 
-print-usage() {
-    log-normal "Usage: create_release.sh <version number> [-p <AWS profile name>] [-b <git branch name>]"
+print_usage() {
+    log_info "Usage: create_release.sh <version number> [-p <AWS profile name>] [-b <git branch name>]"
 }
 
-check-flag-single-use() {
+check_flag_single_use() {
     local flagName="$1"
     local flagVariable="$2"
     if [[ ! -z ${!flagVariable:-} ]]; then
-        log-error "flag $flagName cannot be used multiple times"
-        print-usage
+        log_error "flag $flagName cannot be used multiple times"
+        print_usage
         exit 1
     fi
 }
 
-parse-arguments() {
+parse_arguments() {
     unset SUPPLIED_AWS_PROFILE
     unset GIT_BRANCH
 
     while [[ $# -gt 0 ]]; do
         case "$1" in
         -p | --profile)
-            check-flag-single-use "$1" "SUPPLIED_AWS_PROFILE"
+            check_flag_single_use "$1" "SUPPLIED_AWS_PROFILE"
             SUPPLIED_AWS_PROFILE="$2"
             shift 2
             ;;
         -b | --branch)
-            check-flag-single-use "$1" "GIT_BRANCH"
+            check_flag_single_use "$1" "GIT_BRANCH"
             GIT_BRANCH="$2"
             shift 2
             ;;
@@ -76,42 +79,44 @@ parse-arguments() {
                 continue
             fi
             if [[ $1 =~ ^- ]]; then
-                log-error "unknown option $1"
-                print-usage
+                log_error "unknown option $1"
+                print_usage
                 exit 1
             fi
-            log-error "too many arguments"
-            print-usage
+            log_error "too many arguments"
+            print_usage
             exit 1
             ;;
         esac
     done
 
     if [[ -z ${VERSION_NUMBER:-} ]]; then
-        log-error "version number is required"
-        print-usage
+        log_error "version number is required"
+        print_usage
         exit 1
     fi
 }
 
 init() {
-    parse-arguments "$@"
+    trap 'cleanup' EXIT
+
+    parse_arguments "$@"
 
     VERSION_CHECK_EXPRESSION='^v[0-9]\.[0-9]\.[0-9](-[-.A-z0-9]+)?$'
     if ! echo "$VERSION_NUMBER" | grep -E "$VERSION_CHECK_EXPRESSION" -q; then
-        log-error "illegal version number $VERSION_NUMBER.\n\nPlease use a version number that matches this regular expression: $VERSION_CHECK_EXPRESSION"
+        log_error "illegal version number $VERSION_NUMBER.\n\nPlease use a version number that matches this regular expression: $VERSION_CHECK_EXPRESSION"
         exit 1
     fi
 
     if [[ ! -d $DOCS_REPO ]]; then
-        log-error "../klutchio-website does not exist.\n\nPlease make sure that the klutchio-website repo is cloned into the parent directory of the klutchio repo with that name."
+        log_error "../klutchio-website does not exist.\n\nPlease make sure that the klutchio-website repo is cloned into the parent directory of the klutchio repo with that name."
         exit 1
     fi
 
     local message
     if [[ -z ${SUPPLIED_AWS_PROFILE:-} ]]; then
         if [[ -z ${AWS_PROFILE:-} ]]; then
-            log-normal "Detecting AWS account name..."
+            log_info "Detecting AWS account name..."
             AWS_PROFILE="$(aws configure list | grep "profile" | cut -d ':' -f 2 | awk '{$1=$1};1')"
             export AWS_PROFILE
         fi
@@ -122,88 +127,75 @@ init() {
         message="Using supplied AWS profile $AWS_PROFILE"
     fi
 
-    log-normal "$message for interacting with the ECR registry $ECR_REGISTRY_ADDRESS"
+    log_info "$message for interacting with the ECR registry $ECR_REGISTRY_ADDRESS"
 
-    check-dependency "ko" "crossplane" "git" "aws" "yq" "make"
+    check_dependency "ko" "crossplane" "git" "aws" "yq" "make"
 }
 
-prepare-git-branch() {
-    if [[ -z ${GIT_BRANCH:-} ]]; then
-        log-normal "No git branch specified, using current branch $CURRENT_BRANCH"
-        GIT_BRANCH="$CURRENT_BRANCH"
-        return
-    fi
-
-    log-normal "Using supplied git branch $GIT_BRANCH"
-
-    trap 'git checkout "$CURRENT_BRANCH"' EXIT
-
-    if git branch -a | grep -q -E '^[ *] (remotes/.+/)?'"$GIT_BRANCH"'$'; then
-        log-normal "Supplied git branch $GIT_BRANCH exists, checking it out"
-        git checkout "$GIT_BRANCH"
-        return
-    fi
-
-    ensure-remote-git-branch
-
-    git push --set-upstream "$GIT_REMOTE_BRANCH"
-}
-
-ensure-remote-git-branch() {
-    if [[ -z $(git remote) ]]; then
-        log-error "No git remotes found to track for branch $GIT_BRANCH. Please add a git remote and retry."
+ensure_remote_git_branch() {
+    if ! git remote | grep -q "origin"; then
+        log_error "No git remotes origin found. Please add a git remote with that name and retry."
+        exit 1
     fi
 
     if git branch -a | grep -q -E '^[ *] remotes/origin/'"$GIT_BRANCH"'$'; then
-        GIT_REMOTE_BRANCH="remotes/origin/$GIT_BRANCH"
         return
     fi
 
-    if git branch -a | grep -q -E '^[ *] remotes/.+/'"$GIT_BRANCH"'$'; then
-        GIT_REMOTE_BRANCH="$(git branch -a | grep -E '^[ *] remotes/.+/'"$GIT_BRANCH" | head -n 1)"
-        return
-    fi
-
-    log-normal "No remote branch found for $GIT_BRANCH, creating it"
-
-    local remoteToTrack
-    remoteToTrack="origin"
-    if ! git remote | grep -q "^origin$"; then
-        remoteToTrack="$(git remote | head -n 1)"
-    fi
-
-    git remote set-branches --add "$remoteToTrack" "$GIT_BRANCH"
-    GIT_REMOTE_BRANCH="remotes/$remoteToTrack/$GIT_BRANCH"
+    log_info "No remote branch found for $GIT_BRANCH, creating it"
+    git push --set-upstream origin "$GIT_BRANCH"
 }
 
-build-docker-images() {
-    log-normal "Logging into AWS ECR..."
+setup_git_branch_in_directory() {
+    cd "$1"
+    log_info "Setting up git branch $GIT_BRANCH in directory ${1}..."
+
+    CHECKOUT_FLAG=""
+    if ! (git branch -a | grep -q -E '^[ *] (remotes/.+/)?'"$GIT_BRANCH"'$'); then
+        CHECKOUT_FLAG="-b"
+    fi
+    git stash --all
+    git checkout $CHECKOUT_FLAG "$GIT_BRANCH"
+
+    ensure_remote_git_branch
+}
+
+setup_git_branches() {
+    if [[ -z ${GIT_BRANCH:-} ]]; then
+        GIT_BRANCH="$CURRENT_BRANCH_KLUTCHIO"
+        log_info "No git branch specified, using current branch $CURRENT_BRANCH_KLUTCHIO"
+        ensure_remote_git_branch
+        return
+    fi
+
+    setup_git_branch_in_directory "$KLUTCHIO_REPO"
+    setup_git_branch_in_directory "$DOCS_REPO"
+}
+
+build_docker_images() {
+    log_info "Logging into AWS ECR..."
     aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin "${ECR_REGISTRY_ADDRESS}"
-    log-normal "ECR login successful"
 
-    cd "$KLUTCHIO_REPO/bind"
+    log_info "Building konnector image..."
+    KO_DOCKER_REPO="${ECR_REGISTRY_ADDRESS}/klutch/konnector" ko build "$KLUTCHIO_REPO/bind/cmd/konnector" --bare -t "${VERSION_NUMBER}" --platform=linux/amd64,linux/arm64
 
-    log-normal "Building konnector image..."
-    KO_DOCKER_REPO="${ECR_REGISTRY_ADDRESS}/klutch/konnector" ko build ./cmd/konnector --bare -t "${VERSION_NUMBER}" --platform=linux/amd64,linux/arm64
-    log-normal "konnector image built successfully"
+    log_info "Building example-backend image..."
+    KO_DOCKER_REPO="${ECR_REGISTRY_ADDRESS}/klutch/example-backend" ko build "$KLUTCHIO_REPO/cmd/example-backend" --bare -t "${VERSION_NUMBER}" --platform=linux/amd64,linux/arm64
 
-    log-normal "Building example-backend image..."
-    KO_DOCKER_REPO="${ECR_REGISTRY_ADDRESS}/klutch/example-backend" ko build ./cmd/example-backend --bare -t "${VERSION_NUMBER}" --platform=linux/amd64,linux/arm64
-    log-normal "example-backend image built successfully"
-
+    log_info "Building and pushing dataservices configuration image..."
     make -C "$KLUTCHIO_REPO/crossplane-api" dataservices-config-push dataservicesConfigVersion="${VERSION_NUMBER}"
 
     cd "$KLUTCHIO_REPO/provider-anynines"
-    log-normal "Building provider-anynines controller images..."
+
+    log_info "Building provider-anynines controller images..."
     make buildx.all IMAGETAG="${VERSION_NUMBER}"
-    log-normal "provider-anynines controller images built successfully"
-    log-normal "Building provider-anynines Provider image..."
+
+    log_info "Building provider-anynines Provider image..."
     make provider-build-push IMAGETAG="${VERSION_NUMBER}"
-    log-normal "provider-anynines Provider image built successfully"
 }
 
-update-manifests() {
-    log-normal "Updating version references in klutchio manifests and README.md..."
+update_manifests() {
+    log_info "Updating version references in klutchio manifests and README.md..."
     yq -i ".spec.package = \"$ECR_REGISTRY_ADDRESS/klutch/dataservices:$VERSION_NUMBER\"" \
         "$KLUTCHIO_REPO/crossplane-api/deploy/config-pkg-anynines.yaml"
     yq -i "with(select(document_index == 0); .spec.package = \"$ECR_REGISTRY_ADDRESS/klutch/provider-anynines:$VERSION_NUMBER\")" \
@@ -220,11 +212,71 @@ update-manifests() {
         "$KLUTCHIO_REPO/test/e2e/provider/manifests/install/provider.yaml"
     yq -i ".spec.package = \"$ECR_REGISTRY_ADDRESS/klutch/dataservices:$VERSION_NUMBER\"" \
         "$KLUTCHIO_REPO/test/e2e/provider/manifests/configuration.yaml"
-    log-normal "Klutchio manifest and README.md version references updated to ${VERSION_NUMBER}.\n"
+    log_info "Klutchio manifest and README.md version references updated to ${VERSION_NUMBER}.\n"
 }
 
-update-documentation() {
-    log-normal "Updating version references in klutchio-website documentation repo"
+try_with_tolerated_error_string() {
+    toleratedErrorString="$1"
+    shift
+    set +e
+    output=("$("$@")")
+    returnCode=$?
+    set -e
+    if [[ $returnCode -ne 0 ]]; then
+        if ! echo "${output[*]}" | grep -q -F "$toleratedErrorString"; then
+            log_info "Command $* failed with error:\n${output[*]}"
+            exit 1
+        fi
+    fi
+    echo "${output[@]}"
+}
+
+commit_and_push_changes() {
+    repoName="$1"
+    shift
+
+    log_info "Committing and pushing changes in $repoName with message: $*"
+
+    cd "$repoName"
+    git add --all
+    try_with_tolerated_error_string "nothing to commit" git commit -m "$@"
+    try_with_tolerated_error_string "Everything up-to-date" git push origin "$GIT_BRANCH"
+}
+
+update_changelog() {
+    # The ":1" in the variable expansion instructs the shell to omit the first character (the leading "v") from the version number when inserting it into the changelog.
+    HEADLINE="[${VERSION_NUMBER:1}] - $(date "+%Y-%m-%d")"
+    if grep -x -q -F "## $HEADLINE" "$KLUTCHIO_REPO/CHANGELOG.md"; then
+        log_info "Changelog already contains a section for version $VERSION_NUMBER. Skipping..."
+        return
+    fi
+
+    log_info "Replacing 'Unreleased' changelog section header with header '$HEADLINE'..."
+    sed -E 's/^## Unreleased$/'"## $HEADLINE/g" \
+        "$KLUTCHIO_REPO/CHANGELOG.md" >"$KLUTCHIO_REPO/CHANGELOG.md.tmp"
+    mv "$KLUTCHIO_REPO/CHANGELOG.md.tmp" "$KLUTCHIO_REPO/CHANGELOG.md"
+}
+
+update_klutchio_repo() {
+    update_manifests
+    update_changelog
+    commit_and_push_changes "$KLUTCHIO_REPO" "Prepare version $VERSION_NUMBER"
+    recreate_changelog_unreleased_section
+}
+
+recreate_changelog_unreleased_section() {
+    if grep -q "^## Unreleased$" "$KLUTCHIO_REPO/CHANGELOG.md"; then
+        log_info 'Changelog already contains an "Unreleased" section. Skipping recreation of "Unreleased" section...'
+        return
+    fi
+
+    log_info 'Recreating "Unreleased" section in Changelog...'
+    echo -e "# CHANGELOG\n\n## Unreleased\n$(tail -n +2 "$KLUTCHIO_REPO/CHANGELOG.md")" >"$KLUTCHIO_REPO/CHANGELOG.md"
+    commit_and_push_changes "$KLUTCHIO_REPO" "Recreate Changelog Section for unreleased Changes"
+}
+
+update_documentation_repo() {
+    log_info "Updating version references in klutchio-website documentation repo"
     sed -E 's|klutch/konnector:v[0-9]+\.[0-9]+\.[0-9]+[^ ]*|klutch/konnector:'"${VERSION_NUMBER}"'|g' \
         "$DOCS_REPO/docs/local-deployment-guide.md" >"$DOCS_REPO/docs/local-deployment-guide.md.tmp"
     mv "$DOCS_REPO/docs/local-deployment-guide.md.tmp" "$DOCS_REPO/docs/local-deployment-guide.md"
@@ -252,23 +304,17 @@ update-documentation() {
         "$DOCS_REPO/docs/platform-operator-guide/setting-up-klutch-clusters/control-plane-cluster/index.md.tmp"
     mv "$DOCS_REPO/docs/platform-operator-guide/setting-up-klutch-clusters/control-plane-cluster/index.md.tmp" \
         "$DOCS_REPO/docs/platform-operator-guide/setting-up-klutch-clusters/control-plane-cluster/index.md"
+
+    commit_and_push_changes "$DOCS_REPO" "Update documentation to reference version $VERSION_NUMBER of klutchio components"
 }
 
-update-changelog() {
-    sed -E 's|^## Unreleased$|## '"[${VERSION_NUMBER}] - $(date "+%Y-%m-%d")|g" \
-        "$KLUTCHIO_REPO/CHANGELOG.md" >"$KLUTCHIO_REPO/CHANGELOG.md.tmp"
-    mv "$KLUTCHIO_REPO/CHANGELOG.md.tmp" "$KLUTCHIO_REPO/CHANGELOG.md"
-}
-
-commit-and-push-changes() {
-    git add ./*
-    git commit -m "$@"
-    git push
-}
-
-recreate-changelog-unreleased-section() {
-    echo -e "# CHANGELOG\n\n## Unreleased\n$(tail -n +2 "$KLUTCHIO_REPO/CHANGELOG.md")" >"$KLUTCHIO_REPO/CHANGELOG.md"
-    commit-and-push-changes "Recreate Changelog Section for unreleased Changes"
+cleanup() {
+    log_info "Cleaning up git branches and stashes..."
+    cd "$KLUTCHIO_REPO"
+    git checkout "$CURRENT_BRANCH_KLUTCHIO"
+    cd "$DOCS_REPO"
+    git checkout "$CURRENT_BRANCH_DOCS"
+    git stash apply -q
 }
 
 ################################
@@ -277,11 +323,9 @@ recreate-changelog-unreleased-section() {
 #                              #
 ################################
 
+trap 'git checkout "$CURRENT_BRANCH"' EXIT
 init "$@"
-# build-docker-images
-prepare-git-branch
-update-manifests
-update-documentation
-update-changelog
-commit-and-push-changes "Prepare version $VERSION_NUMBER"
-recreate-changelog-unreleased-section
+build_docker_images
+setup_git_branches
+update_klutchio_repo
+update_documentation_repo
